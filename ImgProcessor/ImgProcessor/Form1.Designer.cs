14153169fb68361e6172f07815790c2400fdbcc0
Wojtek Buchwald <wojtek.buchwald@windowslive.com>
2022-03-13 18:32:48 +0100

    [ADD] Basic part

---
+++ ImgProcessor/ImgProcessor/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace ImgProcessor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OriginalImage = new System.Windows.Forms.PictureBox();
+            this.EditedImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.kamusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Revert = new System.Windows.Forms.Button();
+            this.Inversion = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EditedImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OriginalImage
+            // 
+            this.OriginalImage.Location = new System.Drawing.Point(12, 33);
+            this.OriginalImage.Name = "OriginalImage";
+            this.OriginalImage.Size = new System.Drawing.Size(352, 586);
+            this.OriginalImage.TabIndex = 0;
+            this.OriginalImage.TabStop = false;
+            // 
+            // EditedImage
+            // 
+            this.EditedImage.Location = new System.Drawing.Point(370, 33);
+            this.EditedImage.Name = "EditedImage";
+            this.EditedImage.Size = new System.Drawing.Size(352, 586);
+            this.EditedImage.TabIndex = 1;
+            this.EditedImage.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kamusToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1106, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // kamusToolStripMenuItem
+            // 
+            this.kamusToolStripMenuItem.Name = "kamusToolStripMenuItem";
+            this.kamusToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.kamusToolStripMenuItem.Text = "Load";
+            this.kamusToolStripMenuItem.Click += new System.EventHandler(this.LoadImage);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // Revert
+            // 
+            this.Revert.Location = new System.Drawing.Point(751, 53);
+            this.Revert.Name = "Revert";
+            this.Revert.Size = new System.Drawing.Size(75, 23);
+            this.Revert.TabIndex = 3;
+            this.Revert.Text = "Revert";
+            this.Revert.UseVisualStyleBackColor = true;
+            this.Revert.Click += new System.EventHandler(this.RevertImage);
+            // 
+            // Inversion
+            // 
+            this.Inversion.Location = new System.Drawing.Point(751, 98);
+            this.Inversion.Name = "Inversion";
+            this.Inversion.Size = new System.Drawing.Size(75, 26);
+            this.Inversion.TabIndex = 4;
+            this.Inversion.Text = "Inversion";
+            this.Inversion.UseVisualStyleBackColor = true;
+            this.Inversion.Click += new System.EventHandler(this.InvertImage);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(747, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 25);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "BrightnessChange";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.BrightnessChangeImage);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(741, 184);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 29);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Gamma";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.GammaImage);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(746, 234);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(79, 26);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Contrantenh";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.ContrastCorrectionImage);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(743, 275);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(93, 31);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Blur";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.BlurImage);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(746, 322);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(89, 30);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "Sharpen";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.SharpenImage);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(746, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 50);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Gauss";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.GaussBlurImage);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(746, 438);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Edge detect";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.EdgeDetectImage);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(744, 485);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(109, 32);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Embosis";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.EmbossImage);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1106, 631);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Inversion);
+            this.Controls.Add(this.Revert);
+            this.Controls.Add(this.EditedImage);
+            this.Controls.Add(this.OriginalImage);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.OriginalImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.EditedImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox OriginalImage;
+        private System.Windows.Forms.PictureBox EditedImage;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem kamusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.Button Revert;
+        private System.Windows.Forms.Button Inversion;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ ImgProcessor/ImgProcessor/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgProcessor
+{
+    public partial class Form1 : Form
+    {
+        public int brightness_coeff = 30;
+        public float gamma = 1.2f;
+        public float contrast_coeff = 1.2f;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadImage(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Select a picture";
+            op.Filter = "All supported graphics|*.jpg;*.jpeg;*.png|" +
+              "JPEG (*.jpg;*.jpeg)|*.jpg;*.jpeg|" +
+              "Portable Network Graphic (*.png)|*.png";
+            
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                OriginalImage.Image = System.Drawing.Image.FromFile(op.FileName);
+                EditedImage.Image = System.Drawing.Image.FromFile(op.FileName);
+    
+            }
+        }
+
+        private void RevertImage(object sender, EventArgs e)
+        {
+            EditedImage.Image = OriginalImage.Image;
+        }
+
+        private void InvertImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color pixel = bmp.GetPixel(i, j); 
+                    bmp.SetPixel(i, j, Color.FromArgb(pixel.A , 255 - pixel.R, 255 - pixel.G, 255 - pixel.B));
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void BrightnessChangeImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            int red;
+            int green;
+            int blue;
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    Color pixel = bmp.GetPixel(i, j);
+
+                    if (brightness_coeff > 0)
+                    {
+                        red = pixel.R + brightness_coeff < 255 ? pixel.R + brightness_coeff : 255;
+                        green = pixel.G + brightness_coeff < 255 ? pixel.G + brightness_coeff : 255;
+                        blue = pixel.B + brightness_coeff < 255 ? pixel.B + brightness_coeff : 255;
+                    }
+                    else
+                    {
+                        red = pixel.R + brightness_coeff > 0 ? pixel.R + brightness_coeff : 0;
+                        green = pixel.G + brightness_coeff > 0 ? pixel.G + brightness_coeff : 0;
+                        blue = pixel.B + brightness_coeff > 0 ? pixel.B + brightness_coeff : 0;
+
+                    }
+                    bmp.SetPixel(i, j, Color.FromArgb(pixel.A, red, green, blue));
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void GammaImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+
+                    Color pixel = bmp.GetPixel(i, j);
+                    int red = (int)Math.Pow(pixel.R, gamma) < 255 ? (int)Math.Pow(pixel.R, gamma) : 255 ;
+                    int green = (int)Math.Pow(pixel.G, gamma) < 255 ? (int)Math.Pow(pixel.G, gamma) : 255;
+                    int blue = (int)Math.Pow(pixel.B, gamma) < 255 ? (int)Math.Pow(pixel.B, gamma) : 255;
+
+                    bmp.SetPixel(i, j, Color.FromArgb(pixel.A, red, green, blue));
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void ContrastCorrectionImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+
+                    Color pixel = bmp.GetPixel(i, j);
+                    int red = (pixel.R-128)*contrast_coeff + 128 < 255 ? (int)((pixel.R - 128) * contrast_coeff + 128) : 255;
+                    int green = (pixel.G - 128) * contrast_coeff + 128 < 255 ? (int)((pixel.G - 128) * contrast_coeff + 128) : 255;
+                    int blue = (pixel.B - 128) * contrast_coeff + 128 < 255 ? (int)((pixel.B - 128) * contrast_coeff + 128) : 255;
+                    red = red > 0 ? red : 0;
+                    green = green > 0 ? green : 0;
+                    blue = blue > 0 ? blue : 0;
+                    bmp.SetPixel(i, j, Color.FromArgb(pixel.A, red, green, blue));
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void BlurImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            Bitmap original = new Bitmap(EditedImage.Image);
+            int[] filter = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    bmp.SetPixel(i, j,
+                        Filter(original, filter, i, j, 3)
+                    );
+                
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void GaussBlurImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            Bitmap original = new Bitmap(EditedImage.Image);
+            int[] filter = { 0, 1, 0, 1, 4, 1, 0, 1, 0 };
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    bmp.SetPixel(i, j,
+                        Filter(original, filter, i, j, 3)
+                    );
+
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void SharpenImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            Bitmap original = new Bitmap(EditedImage.Image);
+            int[] filter = { -1, -1, -1, -1, 9, -1, -1, -1, -1};
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    bmp.SetPixel(i, j,
+                        Filter(original, filter, i, j, 3)
+                    );
+
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void EdgeDetectImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            Bitmap original = new Bitmap(EditedImage.Image);
+            int[] filter = { 0, -1, 0, 0, 1, 0, 0, 0, 0 };
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    bmp.SetPixel(i, j,
+                        Filter(original, filter, i, j, 3)
+                    );
+
+                }
+            EditedImage.Image = bmp;
+        }
+
+        private void EmbossImage(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(EditedImage.Image);
+            Bitmap original = new Bitmap(EditedImage.Image);
+            int[] filter = { -1, 0, 1, -1, 1, 1, -1, 0, 1 };
+            for (int i = 0; i < bmp.Width; i++)
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    bmp.SetPixel(i, j,
+                        Filter(original, filter, i, j, 3)
+                    );
+
+                }
+            EditedImage.Image = bmp;
+        }
+        private Color Filter(Bitmap bmp, int[] filter, int x, int y, int kernelsize)
+        {
+            int red = 0;
+            int blue = 0;
+            int green = 0;
+            int imgwidth = bmp.Width;
+            int imgheigth = bmp.Height;
+            int counter_x = -kernelsize / 2;
+            int counter_y = -kernelsize / 2;
+            int divisor = filter.Sum((int n) => { return n;});
+            if(divisor == 0)
+                divisor = 1;
+
+            for (int i = 0; i < kernelsize; ++i) { 
+                for (int j = 0; j < kernelsize; ++j)
+                {
+                    if ( x+counter_x >= imgwidth || y+counter_y >= imgheigth ||
+                         x + counter_x < 0 || y + counter_y < 0)
+                    {
+                        //assuming out of bound pixels are original pixel
+                        Color pixel = bmp.GetPixel(x, y);
+                        red += pixel.R * filter[j * kernelsize + i];
+                        blue += pixel.B * filter[j * kernelsize + i];
+                        green += pixel.G * filter[j * kernelsize + i];
+                    }
+                    else
+                    {
+                        Color pixel = bmp.GetPixel(x+counter_x, y+counter_y);
+                        red += pixel.R * filter[j * kernelsize + i];
+                        blue += pixel.B * filter[j * kernelsize + i];
+                        green += pixel.G * filter[j * kernelsize + i];
+                    }
+
+                    counter_y++;
+                }
+                counter_x++;
+                counter_y = -kernelsize / 2;
+            }
+
+            red = red > 0 ? red : 0;
+            green = green > 0 ? green : 0;
+            blue = blue > 0 ? blue : 0;
+            
+            return Color.FromArgb( red/divisor > 255 ? 255 : red/ divisor,
+                                   green / divisor > 255 ? 255 : green / divisor,
+                                   blue / divisor > 255 ? 255 : blue / divisor);
+
+        }
+    }
+}
